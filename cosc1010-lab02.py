37f5479dd285a586e8548d323e8577ab9ffccd0a
JackZweibelson <jzweibel@uwyo.edu>
2024-10-09 20:32:26 +0000

    HW

--- cosc1010-lab02.py
+++ cosc1010-lab02.py
@@ -1,56 +1,49 @@
-# Jack Zwiebelson
+
+# Jack Zweibelson
 # UWYO COSC 1010
-# 9/19/24
-# Lab 02 
-# Lab Section: 
-# Sources, people worked with, help given to: 
+# 10/9/24
+# HW 01
+# Lab Section:
+# Sources, people worked with, help given to:
+# Chatgpt to give me an indpeth explanation of the importance of .items() while I also was analyzing lecture 9 notes
 # your
 # comments
 # here
+# Homework Question:
+#
+# You are given a list of dictionaries where each dictionary represents a student
+#and their scores
+# in different subjects.
+#
+# Student Data:
+students = [
+ {"name": "Alice", "scores": {"Math": 85, "Science": 90, "English": 78}},
+ {"name": "Bob", "scores": {"Math": 70, "Science": 88, "English": 82}},
+ {"name": "Charlie", "scores": {"Math": 92, "Science": 81, "English": 89}},
+ {"name": "David", "scores": {"Math": 60, "Science": 75, "English": 80}}
+ ]
+#Write a Python program that:
+# 1. Calculates the average score for each student.
 
-your_variable_here = "when you see this, replace it with your code"
+for i in students:
+    avg = sum(i["scores"].values())/len(i["scores"])
+    print(f'{i["name"]} has an average of {avg}')
 
-## Section ONE
+# 2. Stores these averages in a new dictionary where the student’s name is the key
+#and their average score is the value.
 
-# Complete the following print statement to print out "Hello, COSC1010"
-print("Hello, COSC1010")
+grades = {}
 
-# Assign the string above to a variable named hello_message and print that variable
-hello_message = "hello"
-print(hello_message)
+for i in students:
+    avg = sum(i["scores"].values())/len(i["scores"])
+    grades[i["name"]] = avg
+print(grades)
 
-# Assign the string "cowboy joe" to a variable, and print that variable with title casing
-x = "cowboy joe"
-print(x.title())
+# 3. Prints the names of students whose average score is greater than 80.
+# Your task is to calculate the average scores for each student and print the names
+# of students whose average score is greater than 80.
 
-# Complete the following f-string print message 
-    # You will need to create your own variables and insert them  
-    # the final message should read `The University of Wyoming was founded in 1886`
-x = "University of Wyoming"
-y = 1886
-print(f"The {x} was founded in {y}")
+for name, average in grades.items():
+    if average > 80:
+        print(f'{name} has an average greater than 80')
 
-# Now let's do some math with variables 
-    # Create two variables x and y and assign them the values 5 and 10 respectively 
-    # Complete the following print statements using your variables
-    #All math must be done within the braces in the f-strings
-x = 5
-y = 10
-print(f"x + y = {}")
-print(f"x - y = {your_variable_here}")
-print(f"x * y = {your_variable_here}")
-print(f"x / y = {your_variable_here}")
-
-# String concatenation 
-    # Finally we will take a look at string concatenation
-    # String concatenation combines two strings together
-    # It is done using the + operator
-    # Create three variables:
-        # first_name, which is your first name 
-        # last_name, which is your last name
-        # space, which is a space character 
-    # Use string concatenation to print out your full name 
-first_name = "Jack"
-last_name = "Zweibelson"
-space = " "
-print(first_name + space + last_name)
